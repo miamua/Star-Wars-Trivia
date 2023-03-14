5c15e098aa8950c4c3bbb85992ad841429bdedcf
miamua <nanthiporn.aumaim@gmail.com>
2023-03-14 23:52:35 +0100

    added expensive vechicle function

--- main.js
+++ main.js
@@ -11,8 +11,12 @@ class Character {
       movies,
       pictureUrl,
       homeWorld,
-      allMoviesName,
-      movieDates,
+      moviesNames = [],
+      movieDates = [] ,
+      planetName,
+      vehicle = [],
+      starship = [],
+      mostExpensiveCraft = "",
       
     ) {
       this.name = name;
@@ -24,31 +28,83 @@ class Character {
       this.movies = movies;
       this.pictureUrl = pictureUrl;
       this.homeWorld = homeWorld;
-      this.moviesNames = allMoviesName = [];
-      this.getMoviesName();
-      this.movieDate =  movieDates = [];
-      this.getFirstDate();
+      this.moviesNames = moviesNames;
+      this.movieDate = movieDates;
+      this.planetName = planetName;
+      this.vehicle = vehicle;
+      this.starship = starship;
+      this.mostExpensiveCraft = mostExpensiveCraft;
+      
     }
-    async getMoviesName() {
-      this.movies.forEach(async(movie) => {
-          let theMovie = await fetch(movie);
-          let movieName = await theMovie.json();
-          this.moviesNames.push(movieName);
-          
-          })
+    async getMoviesName(){
+          return this.moviesNames;
     }
     async getFirstDate(){
-      this.movies.forEach(async(movie) => {
-        let theMovie = await fetch(movie);
-        let movieDates = await theMovie.json();
-        this.movieDate.push(movieDates.release_date);
-      })
+        let FirstDate = this.movieDate.sort(function(a,b){
+        return Date.parse(a) > Date.parse(b);
+    });
+      return FirstDate;
     }
-
+    async getHomePlanet(){
+      return this.planetName;
+    }
+    async mostExpensiveVehicle(){
+      let pricesOfVehicles = [];
+      this.vehicle.forEach(async(vehicle) => {
+        if (vehicle.cost_in_credits !== "unknown") {
+          pricesOfVehicles.push(vehicle.cost_in_credits);
+        
+      }
+    });
+    console.log(pricesOfVehicles);
+    
+      let pricesOfStarship = [];
+        this.starship.forEach(async(starship) => {
+          if (starship.cost_in_credits !== "unknown") {
+            pricesOfStarship.push(starship.cost_in_credits);
+          }
+        });
+        console.log(pricesOfStarship);
 
     
-  };
+    let theMostexpensiveVehicles = Math.max(...pricesOfVehicles);
+    let theMostexpensiveStarship = Math.max(...pricesOfStarship);
+    let dyrasteFordonsPris = "";
 
+    console.log(`${this.name} most expensive vechicle costs ${Math.max(...pricesOfVehicles)}`);
+    console.log(`${this.name} most expensive starship costs ${Math.max(...pricesOfStarship)}`);
+
+    if(theMostexpensiveVehicles > theMostexpensiveStarship){
+        dyrasteFordonsPris = theMostexpensiveVehicles;
+    }else{
+      dyrasteFordonsPris = theMostexpensiveStarship;
+    }
+    console.log(dyrasteFordonsPris);
+
+    let dyrasteFordon = "";
+    let dyrasteStarskepp = "";
+
+    try {
+        dyrasteFordon = await this.vehicle.find(item => item.cost_in_credits == dyrasteFordonsPris);
+        this.mostExpensiveCraft = dyrasteFordon;
+
+    } catch (error) {
+      console.log(error)
+    }
+    
+    try {
+        dyrasteStarskepp = await this.starship.find(item => item.cost_in_credits == dyrasteFordonsPris);
+        this.mostExpensiveCraft = dyrasteStarskepp;
+    } catch (error) {
+      console.log(error)
+    }
+    
+    
+    //console.log(dyrasteStarskepp);
+    //console.log(dyrasteFordon);
+
+    }
+};
 
 
   const fetchData = async (url) => {
@@ -60,12 +116,60 @@ class Character {
   let selectedChar = [];
   let pageParam = "";
   let allCharacters = [];
+
+  function movieNames(movies) {
+    let movieNames = [];
+    movies.forEach(async(film) => {
+      let theMovie = await fetch(film);
+      let movieName = await theMovie.json();
+      movieNames.push(movieName.title);
+      })
+      return movieNames;
+  }
+
+  function movieDates(movies) {
+    let movieDates = [];
+    movies.forEach(async(film) => {
+      let theMovie = await fetch(film);
+      let movieName = await theMovie.json();
+      movieDates.push(movieName.release_date);
+      })
+      return movieDates;
+  }
   
+  async function homePlanet(home){
+    let planet = await fetch(home);
+    let planetName = await planet.json();
+    return await planetName;
+  }
+
+  async function getVehicle(url){
+    let vehicleArr = [];
+    await url.forEach(async(item) =>{
+    let vehicle = await fetch(item);
+    let allVehicle = await vehicle.json();
+    vehicleArr.push(allVehicle);
+    })
+    return vehicleArr;
+  }
+
+  async function getStarship(url){
+    let starshipArr = [];
+    await url.forEach(async(item) =>{
+    let starship = await fetch(item);
+    let allStarship = await starship.json();
+    starshipArr.push(allStarship);
+    })
+    return starshipArr;
+  }
+
+
+
+
   let getAllchar = async () => {
     for (let i = 1; i < 17; i++) {
-      pageParam = i;
-      console.log(pageParam);
-      let data = await fetchData(`https://swapi.dev/api/people/${pageParam}`);
+      console.log(i);
+      let data = await fetchData(`https://swapi.dev/api/people/${i}`);
       console.log(data);
       /*
     let { name, gender, height, mass, hair_color, skin_color, films, pictureUrl} = data;
@@ -79,6 +183,21 @@ class Character {
       let charMovies = films;
       let pictureUrl = `${charName}+.webp`;
       */
+      let getMovieNames = movieNames(await data.films);
+      console.log(getMovieNames)
+
+      let getMovieDates = movieDates(await data.films);
+      console.log(getMovieDates)
+
+      let getHomeWorld = await homePlanet(data.homeworld);
+      console.log(getHomeWorld);
+     
+      let vehicles = await getVehicle(data.vehicles);
+      console.log(vehicles);
+
+      let starship = await getStarship(data.starships);
+      console.log(starship);
+
       let pictureUrl = data.name+`.jpg`;
       let newChar = new Character(
         /*
@@ -90,7 +209,7 @@ class Character {
         charSkin,
         charMovies
         */
-       data.name, data.gender, data.height, data.mass, data.hair_color, data.skin_color, data.films, pictureUrl, data.homeworld,
+       data.name, data.gender, data.height, data.mass, data.hair_color, data.skin_color, data.films, pictureUrl, data.homeworld, getMovieNames, getMovieDates, getHomeWorld.name, vehicles, starship
       );
       
 
@@ -109,7 +228,9 @@ class Character {
 
   };
 
-  //let container = document.querySelector(".profileContainer");
+  
+
+  let container = document.querySelector(".profileContainer");
   let char1Div = document.querySelector("#char1");
   let char1PicDiv = document.createElement("div");
   let char1InfoDiv = document.createElement("div");
@@ -126,7 +247,7 @@ class Character {
        
     char1Div.append(char1PicDiv);
     char2Div.append(char2PicDiv);
-    
+
   };
 
 
@@ -137,7 +258,7 @@ class Character {
   selectedChar = [];
   //compareBtn.style.display = "inherit";
   compareBtn.disabled = false;
-  movieNameBtn.disabled = false;
+  //movieNameBtn.disabled = false;
   //firstAppearBtn.disabled = false;
   //movieNameBtn.style.display = "inherit";
   
@@ -279,57 +400,99 @@ let compare = () =>{
 
 
 let movieNameBtn = document.querySelector("#moviesName");
-movieNameBtn.addEventListener("click", () => {
-    let moivesName1 = document.createElement("div");
-    let moivesName2 = document.createElement("div");
-    
+movieNameBtn.addEventListener("click", async () => {
+   //await selectedChar[0].getMoviesName();
+   //await selectedChar[1].getMoviesName();
+   //console.log(char1MoviesArr[0]);
+   //console.log(char2MoviesArr);
+  
+  let moivesName1 = document.createElement("div");
+  let moivesName2 = document.createElement("div");
+  moivesName1.setAttribute("id","char1MoviesDiv");
+  moivesName2.setAttribute("id","char2MoviesDiv");
+
     selectedChar[0].moviesNames.forEach((movie) => {
       let renderMoviesName1 = document.createElement("ul");
-      renderMoviesName1.innerHTML = `<li>${movie.title}</li>`;
-      console.log(`Char1 movies: ${movie.title}`);
+      renderMoviesName1.innerHTML = `<li>${movie}</li>`;
+      console.log(`Char1 movies: ${movie}`);
       moivesName1.append(renderMoviesName1);  
     });
 
     selectedChar[1].moviesNames.forEach((movie) => {
       let renderMoviesName2 = document.createElement("ul");
-      renderMoviesName2.innerHTML = `<li>${movie.title}</li>`;
-      console.log(`Char2 movies: ${movie.title}`);
+      renderMoviesName2.innerHTML = `<li>${movie}</li>`;
+      console.log(`Char2 movies: ${movie}`);
       moivesName2.append(renderMoviesName2);
     });
     
     char1Div.append(moivesName1);
     char2Div.append(moivesName2); 
 
-    movieNameBtn.disabled = true;
+
+    //movieNameBtn.disabled = true;
+    
+    
     
   });
 
   let firstAppearBtn = document.querySelector("#moviesDate");
-  firstAppearBtn.addEventListener("click", () =>{
-    //let char1DatDiv = document.createElement("div");
-    //let char2DatDiv = document.createElement("div");
+  firstAppearBtn.addEventListener("click", async () =>{
     
-    let char1FirstDate = selectedChar[0].movieDate.sort(function(a,b){
-      return Date.parse(a) > Date.parse(b);
-    });
-    console.log(`${char1FirstDate[0]}`);
+    let char1FirstDate = await selectedChar[0].getFirstDate();
+    let char2FirstDate = await selectedChar[1].getFirstDate();
+
+    console.log(`${char1FirstDate}`);
     let char1DateText = document.createElement("p");
     char1DateText.innerText = `${selectedChar[0].name} first appeared in a movie ${char1FirstDate[0]}`;
 
-
-    let char2FirstDate = selectedChar[1].movieDate.sort(function(a,b){
-      return Date.parse(a) > Date.parse(b);
-    });
-    console.log(`${char2FirstDate[0]}`);
+    
+    console.log(`${char2FirstDate}`);
     let char2DateText = document.createElement("p");
     char2DateText.innerText = `${selectedChar[1].name} first appeared in a movie ${char2FirstDate[0]}`;
 
-    
     char1Div.append(char1DateText);
     char2Div.append(char2DateText);
 
   });
   
+let homePlanetBtn = document.querySelector("#planet");
+homePlanetBtn.addEventListener("click", async () => {
 
+  let char1Home = await selectedChar[0].getHomePlanet();
+  let char2Home = await selectedChar[1].getHomePlanet();
+
+  console.log(`${char1Home}`);
+  let char1HomeText = document.createElement("p");
+  char1HomeText.innerText = `${selectedChar[0].name} lives in ${char1Home}.`;
+
+  console.log(`${char2Home}`);
+  let char2HomeText = document.createElement("p");
+  char2HomeText.innerText = `${selectedChar[1].name} lives in ${char2Home}.`;
+
+  if(char1Home=== char2Home){
+    let samePlanetText = document.createElement("p");
+    console.log(`${selectedChar[0].name} and ${selectedChar[1].name} live in the same Planet!`);
+    samePlanetText.innerText = `${selectedChar[0].name} and ${selectedChar[1].name} live in the same Planet!`;
+    container.append(samePlanetText);
+  }else{
+    console.log(`not same planet`)
+  }
+
+  char1Div.append(char1HomeText);
+  char2Div.append(char2HomeText);
+});
 
  
+let vehicleBtn = document.querySelector("#expensiveVehicle");
+vehicleBtn.addEventListener("click", async () =>{
+
+  await selectedChar[0].mostExpensiveVehicle();
+  await selectedChar[1].mostExpensiveVehicle();
+
+  console.log(selectedChar[0].mostExpensiveCraft);
+  console.log(selectedChar[0].mostExpensiveCraft.name);
+  console.log(selectedChar[1].mostExpensiveCraft);
+  console.log(selectedChar[1].mostExpensiveCraft.name);
+
+
+})
